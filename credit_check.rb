fed042babbcc7f9c1540a233615e8d3cac3672f4
Adam Caron <adamwcaron@gmail.com>
2015-05-31 14:25:05 -0600

    adds a 'check number' and, from the rightmost digit moving left, doubles the value over every second digit

--- credit_check.rb
+++ credit_check.rb
@@ -4,11 +4,31 @@ valid = false
 
 # Your Luhn Algorithm Here
 
-digits = card_number.chars.map.each do |digit|
-  digit.to_i
-end
-puts "The array is #{digits.inspect}"
+digits = card_number.chars.map.each {|digit| digit.to_i}
+digits.push("x") # append a check digit
+# From the rightmost digit (wich is the check number: x), moving left, doublw the value of every second digit.
+puts "Original array: #{digits.inspect}"
+digits.reverse!
+puts "Reversed array: #{digits.inspect}"
+every_other = []
 
+digits.each_index do |d|
+  if d.odd?
+    every_other << digits[d] * 2
+  else
+    every_other << digits[d]
+  end
+end
+puts "New array: #{every_other.inspect}"
+
+# print digits.values_at(* digits.each_index.select { |i| i.odd? }).map! { |n| n * 2 }
+
+# digits.values_at(* digits.each_index.select do |i|
+#   i.odd?
+#   i * 2
+# end )
+
+# digits.values_at(* digits.each_index.map!)
 
 
 # Output
